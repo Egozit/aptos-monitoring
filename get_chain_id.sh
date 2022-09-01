b4ad665086564fbaa8f6462ede3fcfa7d28acc01
Egor Petrov <iampetroved@gmail.com>
2022-09-01 19:41:51 +0300

    Delete get_chain_id.sh

--- get_chain_id.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-chain_id=`curl http://localhost:80/v1 | jq .chain_id`
-cat << EOF | curl --data-binary @- http://localhost:9091/metrics/job/aptos_chain_id
-  aptos_chain_id $chain_id
-EOF
